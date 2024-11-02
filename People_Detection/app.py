5b3b86039a086e3c2a8252c34d4fb17adb92602d
Ömer Faruk Subaşı <71149988+omerfaruksubasi@users.noreply.github.com>
2024-11-02 22:22:35 +0300

    Update app.py

--- People_Detection/app.py
+++ People_Detection/app.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import cv2
 
 model = YOLO('yolov8n.pt')
-video_path = '/home/omer_faruk_subasi/Desktop/docker/people_detection/people.mov'
+video_path = 'people.mmp4'
 cap = cv2.VideoCapture(video_path)
 
 frame_width = int(cap.get(3))
@@ -20,7 +20,7 @@ while cap.isOpened():
         x1,y1,x2,y2 = map(int, result.xyxy[0])
         cv2.rectangle(frame,(x1,y1),(x2,y2),(255,0,0),2)
 
-    out.write(frame)
+out.write(frame)
 cap.release()
 out.release()
 cv2.destroyAllWindows()
